ff21f1e99cbdfc33ab2691d2e22910f67da453ba
George-Kibe <georgekibewambui@gmail.com>
2024-03-21 16:27:54 +0300

    database testing config-TDD

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,6 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #project
+    'core',
+    #external
+    
 ]
 
 MIDDLEWARE = [
