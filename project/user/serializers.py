3ecd1e0e749c71ed77b0c70cd7bdc87a8658a99d
George-Kibe <georgekibewambui@gmail.com>
2024-03-22 19:47:35 +0300

    me url and view

--- project/user/serializers.py
+++ project/user/serializers.py
@@ -1,6 +1,7 @@
 """Serializers for the user APIView"""
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -26,3 +27,27 @@ class UserSerializer(serializers.ModelSerializer):
 
         return user
 
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            request=self.context.get("request"),
+            username=email,
+            password=password
+        )
+        if not user:
+            message = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(message, code="authentication")
+
+        attrs["user"] = user
+        return attrs
--- project/user/views.py
+++ project/user/views.py
@@ -1,7 +1,9 @@
 """View for the user API."""
 
 from rest_framework import generics, authentication, permissions
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -9,3 +11,19 @@ class CreateUserView(generics.CreateAPIView):
 
     serializer_class = UserSerializer
 
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
